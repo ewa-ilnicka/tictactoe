eb9653cbad7e4ca8575775e8ea441f9159534307
unknown <ewa.h.ilnicka@gmail.com>
2020-08-07 10:24:48 +0200

    Added pseudoelement before to boeard__item

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const fields = document.querySelectorAll(".board__item");
 
+let activePlayer = "X";
+
 fields.forEach(field => {
     field.addEventListener("click", (e) => {
-
+        e.target.classList.add(`board__item--filled-$(activePlayer)`)
     })
 })
