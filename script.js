602a4a3d3a807581de23ec44c00f35cbdccca266
unknown <ewa.h.ilnicka@gmail.com>
2020-08-07 15:35:43 +0200

    Now you can't draw X on O and O on X.

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-const fields = document.querySelectorAll(".board__item");
+const fieldsElement = document.querySelectorAll(".board__item");
+
+const fields = ["", "", "", "", "", "", "", "", ""];
 
 let activePlayer = "X";
 
-fields.forEach(field => {
-    field.addEventListener("click", (e) => {
-        e.target.classList.add(`board__item--filled-${activePlayer}`);
-        activePlayer = activePlayer === 'X' ? 'O' : 'X';
+fieldsElement.forEach(field => {
+    field.addEventListener("click", e => {
+        const {
+            pos
+        } = e.target.dataset;
+
+        if (fields[pos] === "") {
+            fields[pos] = activePlayer;
+            e.target.classList.add(`board__item--filled-${activePlayer}`);
+            activePlayer = activePlayer === 'X' ? 'O' : 'X';
+        }
     });
-})
+});
